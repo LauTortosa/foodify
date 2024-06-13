677e46eea770c8e2043353b38507d4534973d1a5
LauTortosa <lau.tortosa@gmail.com>
2024-06-13 11:37:59 +0200

    add unit test for ProductComponent model

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class ProductModelsTests(TestCase):
+
+    def test_ProductComponent_creation(self):
+        type = Type.objects.create(
+            value="PASTA",
+            label="Pasta"
+        )
+        product = Product.objects.create(
+            product="Boloñesa",
+            type=type
+        )
+        component = Component.objects.create(
+            value="TOMATE",
+            label="Tomate",
+        )
+        productComponent = ProductComponent.objects.create(
+            product=product,
+            component=component,
+            kilograms=0.100
+        )
+
+        self.assertEqual(productComponent.product, product)
+        self.assertEqual(productComponent.component, component)
+        self.assertEqual(productComponent.kilograms, 0.100)
