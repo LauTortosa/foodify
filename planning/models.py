9227c794f3e7da410117b5cc0ada0964e1473b11
LauTortosa <lau.tortosa@gmail.com>
2024-06-13 12:13:25 +0200

    add __str__ methods for models

--- planning/models.py
+++ planning/models.py
@@ -7,9 +7,15 @@ class State(models.Model):
     value = models.CharField(max_length=MAX_LENGTH)
     label = models.CharField(max_length=MAX_LENGTH)
 
+    def __str__(self):
+        return f"({self.label})"
+
 class Planning(models.Model):
     date = models.DateField()
     load = models.IntegerField()
     tracebility = models.IntegerField()
     state = models.ForeignKey(State, on_delete=models.RESTRICT)
     product = models.ForeignKey(Product, on_delete=models.RESTRICT)
+
+    def __str__(self):
+        return f"({self.id}) {self.product} {self.state}"
--- product/models.py
+++ product/models.py
@@ -6,13 +6,22 @@ class Type(models.Model):
     value = models.CharField(max_length=MAX_LENGTH)
     label = models.CharField(max_length=MAX_LENGTH)
 
+    def __str__(self):
+        return f"{self.label}"
+
 class Product(models.Model):
     product = models.CharField(max_length=MAX_LENGTH)
     type = models.ForeignKey(Type, on_delete=models.RESTRICT, null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.product} - {self.type}"
+
 class Component(models.Model):
     value = models.CharField(max_length=MAX_LENGTH)
     label = models.CharField(max_length=MAX_LENGTH)
+
+    def __str__(self): 
+        return f"{self.label}"
 
 class ProductComponent(models.Model):
     product = models.ForeignKey(Product, on_delete=models.RESTRICT)
