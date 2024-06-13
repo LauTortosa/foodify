d96f9eecbf20877dd718ad2da4b535378a943383
LauTortosa <lau.tortosa@gmail.com>
2024-06-13 11:27:22 +0200

    add unit test for Planning model

--- planning/tests.py
+++ planning/tests.py
@@ -1,3 +1,27 @@
+from datetime import date
 from django.test import TestCase
+from .models import Planning, State, Product
 
-# Create your tests here.
+class PlanningModelsTests(TestCase):
+
+    def test_planning_creation(self):
+        state = State.objects.create(
+            value="PENDIENTE",
+            label="Pendiente"
+        )
+        product = Product.objects.create(
+            product="Boloñesa",
+        )
+        planning = Planning.objects.create(
+            date=date(2024, 6, 15),
+            load=10,
+            tracebility=250,
+            state=state,
+            product=product
+        )
+
+        self.assertEqual(planning.date, date(2024, 6, 15))
+        self.assertEqual(planning.load, 10)
+        self.assertEqual(planning.tracebility, 250)
+        self.assertEqual(planning.state, state)
+        self.assertEqual(planning.product, product)
