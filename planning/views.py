6c94cd9011134be84f8db89fe6fbe19a4d261c15
Laura <lau.tortosa@gmail.com>
2024-06-15 14:44:25 +0200

    add create_planning view in planning

--- planning/views.py
+++ planning/views.py
@@ -1,6 +1,24 @@
-from ninja import NinjaAPI
+from ninja import NinjaAPI, Schema
+from typing import Optional
+from .models import Planning
+from product.models import Product
+from datetime import date
 
 api = NinjaAPI()
+
+class PlanningIn(Schema):
+    date: str
+    load: int
+    tracebility: int
+    product: str
+
+@api.post("/planning")
+def create_planning(request, data: PlanningIn):
+    planning = Planning.objects.create(date=data.date, load=data.load, tracebility=data.tracebility)
+    planning.product = Product.object.get(product=data.product)
+    planning.save()
+
+    return {"ok": True}
 
 @api.get("/items")
 def list_items(request):
