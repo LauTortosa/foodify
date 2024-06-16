f095f038c1e541758acdad025d444bc7d9708a5d
LauTortosa <lau.tortosa@gmail.com>
2024-06-16 07:40:27 +0200

    add get_planning views in planning

--- planning/views.py
+++ planning/views.py
@@ -1,7 +1,7 @@
 from ninja import NinjaAPI, Schema
-from typing import List
+from typing import List, Optional
 from .models import Planning
-from product.models import Product
+from product.models import Product, ProductComponent
 from datetime import date
 
 api = NinjaAPI()
@@ -19,6 +19,7 @@ class PlanningOut(Schema):
     tracebility: int
     state_value: str
     product_value: str
+    component_value: Optional[List[str]] = None
 
 @api.post("/planning")
 def create_planning(request, data: PlanningIn):
@@ -39,4 +40,14 @@ def list_registered(request):
     planning_registered = Planning.objects.filter(state=3)
 
     return planning_registered
+
+@api.get("planning/{planning_id}", response=PlanningOut)
+def get_planning(request, planning_id: int):
+    planning = Planning.objects.get(id=planning_id)
+    product = planning.product
+    product_components = ProductComponent.objects.filter(product=product)
+    component_values = [f"{pc.component.label} - {pc.kilograms} kg" for pc in product_components]
+    planning.component_value = component_values
+
+    return planning
 
