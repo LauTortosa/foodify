13ba019af529a0b55fa66ccd7ede2d0efb43eb0f
LauTortosa <lau.tortosa@gmail.com>
2024-06-22 07:27:11 +0200

    add 'get_task' view in task

--- task/views.py
+++ task/views.py
@@ -6,11 +6,28 @@ from .models import Task
 task_api = NinjaAPI(urls_namespace='task_api')
 
 class TaskIn(Schema):
-    task: str
+    description: str
+
+class TaskOut(Schema):
+    id: int
+    description: str
+    completed: bool
 
 @task_api.post("/")
 def create_task(request, data: TaskIn):
-    task = Task.objects.create(task=data.task)
+    task = Task.objects.create(task=data.description)
     task.save()
 
     return {"ok": True}
+
+@task_api.get("/{task_id}", response=TaskOut)
+def get_task(request, task_id: int):
+    task = Task.objects.get(id=task_id)
+    description = task.task 
+    completed = task.completed 
+
+    return TaskOut(
+        id=task.id,
+        description=description,
+        completed=completed
+    )
