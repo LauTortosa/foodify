584524f58ccd61ea7d0f8a90fbe7e74d24dd1517
LauTortosa <lau.tortosa@gmail.com>
2024-07-12 07:07:41 +0200

    add grid-cols in CalculateView

--- frontend/src/views/CalculateView.jsx
+++ frontend/src/views/CalculateView.jsx
@@ -1,6 +1,16 @@
 const CalculateView = () => {
     return (
-        <h1>Calculate view</h1>
+        <div class="grid grid-cols-4 divide-x-2 mt-12">
+            <div class="ml-8 mr-4 bg-orange-200">
+            <p>form</p>
+            </div>
+            <div class="col-span-3 overflow-x-auto mb-12 mr-8 bg-green-200">
+            <p>tabla</p>
+            </div>
+
+            
+        </div>
+
     );
 };
 
