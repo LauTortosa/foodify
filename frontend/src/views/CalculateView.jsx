8956e215cd09486da16f45f492260352f65ed8ec
LauTortosa <lau.tortosa@gmail.com>
2024-07-14 08:43:14 +0200

    fix: update calculateTotal to accumulate values

--- frontend/src/views/CalculateView.jsx
+++ frontend/src/views/CalculateView.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 const CalculateView = () => {
     const [types, setTypes] = useState([]);
     const [productsByType, setProductsByType] = useState({});
+    const [selectedProducts, setSelectedProducts] = useState([]);
+    const [calculatedKilosTotal, setCalculatedKilosTotal] = useState([]);
+    const [load, setLoad] = useState(1);
 
     useEffect(() => {
         getTypes();
@@ -35,46 +38,129 @@ const CalculateView = () => {
             setProductsByType(updatedProductsByType);
     };
 
+    const getProductComponents = async (productId) => {
+        const response = await axios.get(`http://localhost:8000/product/api/${productId}`);
+        return response.data.component_value;
+    };
+
     const handleCheckboxChange = (typeId, productId) => {
-        setProductsByType({
-            ...productsByType,
-            [typeId]: productsByType[typeId].map(product => {
+        setProductsByType(prevProductsByType => {
+            const updatedProducts = prevProductsByType[typeId].map(product => {
                 if (product.id === productId) {
                     return {
                         ...product,
-                        checked: !product.checked 
+                        checked: !product.checked
                     };
                 }
                 return product;
-            })
+            });
+            return {
+                ...prevProductsByType,
+                [typeId]: updatedProducts
+            };
         });
-        console.log(productsByType)
+
+        setSelectedProducts(prevSelectedProducts => {
+        if (prevSelectedProducts.includes(productId)) {
+                return prevSelectedProducts.filter(id => id !== productId);
+            } else {
+                return [...prevSelectedProducts, productId];
+            }
+        });
     };
 
+    const calculateTotal = async () => {
+        const newCalculatedKilosComponent = { ...calculatedKilosTotal };
+    
+        for (const productId of selectedProducts) {
+            const components = await getProductComponents(productId);
+    
+            components.forEach((component) => {
+                const [name, kilos] = component.split(" = ");
+                const parsedKilos = parseFloat(kilos) * load;
+    
+                if (!newCalculatedKilosComponent[name]) {
+                    newCalculatedKilosComponent[name] = 0;
+                }
+    
+                newCalculatedKilosComponent[name] += parsedKilos;
+            });
+        }
+    
+        setCalculatedKilosTotal(newCalculatedKilosComponent);
+        console.log('calc', newCalculatedKilosComponent);
+    };
+    
+
+    useEffect(() => {
+        console.log('Selected Products:', selectedProducts);
+    }, [selectedProducts]);
+
     return (
-        <div>
-            {types.map(type => (
-                <div key={type.id}>
-                    <h3 className="font-bold">{type.label}</h3>
-                    <ul>
-                        {productsByType[type.id]?.map(product => (
-                            <li key={product.id}>
-                                <label className="flex items-center space-x-2">
-                                    <input
-                                        type="checkbox"
-                                        checked={product.checked}
-                                        onChange={() => handleCheckboxChange(type.id, product.id)}
-                                        className="checkbox"
-                                    />
-                                    <span>{product.product}</span>
-                                </label>
-                            </li>
-                        ))}
-                    </ul>
+        <div className='grid grid-cols-4 divide-x-2 mt-12'>
+            <div className='ml-8 mr-4'>
+                <div>
+                    {types.map(type => (
+                        <div key={type.id}>
+                            <h3 className="font-bold">{type.label}</h3>
+                            <ul>
+                                {productsByType[type.id]?.map(product => (
+                                    <li key={product.id}>
+                                        <label className="flex items-center space-x-2">
+                                            <input
+                                                type="checkbox"
+                                                checked={product.checked}
+                                                onChange={() => handleCheckboxChange(type.id, product.id)}
+                                                className="checkbox"
+                                            />
+                                            <span>{product.product}</span>
+                                        </label>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
                 </div>
-            ))}
+                <div className="mt-8 ml-4">
+                    <label className="text-sm mt-8 mr-4 font-bold">
+                        Número de cargas
+                    </label>
+                    <input
+                        type="number"
+                        className="input input-bordered input-sm w-40 max-w-xs mt-2 mb-2"
+                        v-model="{load}" 
+                        onChange={(e) => setLoad(parseInt(e.target.value))}
+                    />
+                    <button 
+                        onClick={calculateTotal}
+                        className="btn w-40 mt-6 mb-8">
+                        Calcular
+                    </button>
+                </div>
+            </div>
+         <div className="col-span-3 overflow-x-auto mb-12 mr-8">
+         <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Componente</th>
+                            <th>Total kilos</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {Object.entries(calculatedKilosTotal).map(([componentName, totalKilos], index) => (
+                            <tr key={componentName} className="mb-4">
+                                <td>{index + 1}</td>
+                                <td>{componentName}</td>
+                                <td>{totalKilos} kg</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+         </div>
         </div>
     );
 };
 
 export default CalculateView;
+
