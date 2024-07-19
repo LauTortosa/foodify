8218acc4057757aa2acf93175b9fddcf879103e7
LauTortosa <lau.tortosa@gmail.com>
2024-07-19 16:34:11 +0200

    refact: add ChecksProductsComponent and refactor CalculateView

---
+++ frontend/src/components/Calculate/ChecksProductsComponent.jsx
@@ -0,0 +1,92 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const ChecksProductsComponent = ({ selectedProducts, setSelectedProducts }) => {
+    const [types, setTypes] = useState([]);
+    const [productsByType, setProductsByType] = useState({});
+
+    useEffect(() => {
+        getTypes();
+    }, []);
+
+    const getTypes = async () => {
+        const response = await axios.get('http://localhost:8000/product/api/types');
+        const typesData = response.data;
+        setTypes(typesData);
+
+        const productsData = await Promise.all(
+            typesData.map(async (type) => {
+                const productsResponse = await axios.get(`http://localhost:8000/product/api/products?type_id=${type.id}`);
+                return {
+                    typeId: type.id,
+                    products: productsResponse.data
+                };
+            })
+        );
+
+        // organice products by type
+        if (Array.isArray(productsData)) {
+            const updatedProductsByType = {};
+            productsData.forEach(({ typeId, products }) => {
+                updatedProductsByType[typeId] = products.map(product => ({
+                    ...product,
+                    checked: false
+                }));
+            });
+            setProductsByType(updatedProductsByType);
+        }
+    };
+
+    const handleCheckboxChange = (typeId, productId) => {
+        setProductsByType(prevProductsByType => {
+            const updatedProducts = prevProductsByType[typeId].map(product => {
+                if (product.id === productId) {
+                    return {
+                        ...product,
+                        checked: !product.checked
+                    };
+                }
+                return product;
+            });
+            return {
+                ...prevProductsByType,
+                [typeId]: updatedProducts
+            };
+        });
+
+        setSelectedProducts(prevSelectedProducts => {
+        if (prevSelectedProducts.includes(productId)) {
+                return prevSelectedProducts.filter(id => id !== productId);
+            } else {
+                return [...prevSelectedProducts, productId];
+            }
+        });
+    };
+
+    return (
+        <div>
+            {types.map(type => (
+                <div key={type.id}>
+                    <h3 className="font-bold">{type.label}</h3>
+                    <ul>
+                        {productsByType[type.id]?.map(product => (
+                            <li key={product.id}>
+                                <label className="flex items-center space-x-2">
+                                    <input
+                                        type="checkbox"
+                                        checked={product.checked}
+                                        onChange={() => handleCheckboxChange(type.id, product.id)}
+                                        className="checkbox"
+                                    />
+                                    <span>{product.product}</span>
+                                </label>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    )
+};
+
+export default ChecksProductsComponent;
--- frontend/src/views/CalculateView.jsx
+++ frontend/src/views/CalculateView.jsx
@@ -1,72 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+import ChecksProductsComponent from '../components/Calculate/ChecksProductsComponent';
+
 const CalculateView = () => {
-    const [types, setTypes] = useState([]);
-    const [productsByType, setProductsByType] = useState({});
     const [selectedProducts, setSelectedProducts] = useState([]);
     const [calculatedKilosTotal, setCalculatedKilosTotal] = useState([]);
     const [load, setLoad] = useState(1);
 
-    useEffect(() => {
-        getTypes();
-    }, []);
-
-    const getTypes = async () => {
-            const response = await axios.get('http://localhost:8000/product/api/types');
-            const typesData = response.data;
-            setTypes(typesData);
-
-            const productsData = await Promise.all(
-                typesData.map(async (type) => {
-                    const productsResponse = await axios.get(`http://localhost:8000/product/api/products?type_id=${type.id}`);
-                    return {
-                        typeId: type.id,
-                        products: productsResponse.data
-                    };
-                })
-            );
-            // organice products by type
-            const updatedProductsByType = {};
-            productsData.forEach(({ typeId, products }) => {
-                updatedProductsByType[typeId] = products.map(product => ({
-                    ...product,
-                    checked: false 
-                }));
-            });
-
-            setProductsByType(updatedProductsByType);
-    };
-
     const getProductComponents = async (productId) => {
         const response = await axios.get(`http://localhost:8000/product/api/${productId}`);
         return response.data.component_value;
-    };
-
-    const handleCheckboxChange = (typeId, productId) => {
-        setProductsByType(prevProductsByType => {
-            const updatedProducts = prevProductsByType[typeId].map(product => {
-                if (product.id === productId) {
-                    return {
-                        ...product,
-                        checked: !product.checked
-                    };
-                }
-                return product;
-            });
-            return {
-                ...prevProductsByType,
-                [typeId]: updatedProducts
-            };
-        });
-
-        setSelectedProducts(prevSelectedProducts => {
-        if (prevSelectedProducts.includes(productId)) {
-                return prevSelectedProducts.filter(id => id !== productId);
-            } else {
-                return [...prevSelectedProducts, productId];
-            }
-        });
     };
 
     const calculateTotal = async () => {
@@ -108,32 +52,15 @@ const CalculateView = () => {
         setSelectedProducts([]);
         setCalculatedKilosTotal({});
     }
+    
     // TODO validation form
     return (
         <div className='grid grid-cols-4 divide-x-2 mt-12'>
             <div className='ml-8 mr-4'>
-                <div>
-                    {types.map(type => (
-                        <div key={type.id}>
-                            <h3 className="font-bold">{type.label}</h3>
-                            <ul>
-                                {productsByType[type.id]?.map(product => (
-                                    <li key={product.id}>
-                                        <label className="flex items-center space-x-2">
-                                            <input
-                                                type="checkbox"
-                                                checked={product.checked}
-                                                onChange={() => handleCheckboxChange(type.id, product.id)}
-                                                className="checkbox"
-                                            />
-                                            <span>{product.product}</span>
-                                        </label>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
-                </div>
+                <ChecksProductsComponent
+                    selectedProducts={selectedProducts} 
+                    setSelectedProducts={setSelectedProducts} 
+                />
                 <div className="mt-8 ml-4">
                     <label className="text-sm mt-8 mr-4 font-bold">
                         Número de cargas
@@ -141,7 +68,7 @@ const CalculateView = () => {
                     <input
                         type="number"
                         className="input input-bordered input-sm w-40 max-w-xs mt-2 mb-2"
-                        v-model="{load}" 
+                        value={load}
                         onChange={(e) => setLoad(parseInt(e.target.value))}
                     />
                     <button 
@@ -156,8 +83,8 @@ const CalculateView = () => {
                     </button>
                 </div>
             </div>
-         <div className="col-span-3 overflow-x-auto mb-12 mr-8">
-         <table className="table">
+            <div className="col-span-3 overflow-x-auto mb-12 mr-8">
+                <table className="table">
                     <thead>
                         <tr>
                             <th>#</th>
@@ -175,10 +102,9 @@ const CalculateView = () => {
                         ))}
                     </tbody>
                 </table>
-         </div>
+            </div>
         </div>
     );
 };
 
 export default CalculateView;
-
