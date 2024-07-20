baad08178a2ace5316080c85e9bfb6f8a094562a
LauTortosa <lau.tortosa@gmail.com>
2024-07-20 20:40:43 +0200

    feat: display pending state cunt in HomeView - add getStatePending function to fetch pending state count from API - Display the count of pending state in HomeView

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -1,7 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const HomeView = () => {
+    const [statePending, setStatePending] = useState(0);
+
+    useEffect(() => {
+        getStatePending();
+    }, []);
+
+    const getStatePending = async() => {
+        const response = await axios.get('http://localhost:8000/planning/api/planning/state/pending');
+        setStatePending(response.data["planning pending"]);
+    }
+
     return (
-        <div> home
+        <div className='container mx-auto mt-10'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4'>
+          <div className='md:col-span-2 lg:col-span-1'>
+              <h2 className='text-center text-xl font-bold underline mb-4'>Planificaciones pendientes</h2>
+              <ul>
+                <li>Por preparar {statePending}</li>
+                <li>Por registrar {statePending}</li>
+              </ul>
+          </div>
+          <div className='md:col-span-2 lg:col-span-3 lg:ml-24'>
+              <h2 className="text-center text-xl font-bold underline mb-4">Lista de tareas</h2>
+          </div>
         </div>
+      </div>
     );
 }
 
