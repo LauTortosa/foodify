4064796117ef691f6fa8f3181437efc7272c11c5
LauTortosa <lau.tortosa@gmail.com>
2024-07-21 08:29:37 +0200

    feat: add get task list add 'getTask' function to load tasks from API display tasks in a table in 'HomeView' implement event 'handleCheckboxChange' to toggle the 'completed' state of tasks

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -1,7 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import StatePendingComponent from "../components/Home/StatePendingComponent";
 import StatePreparedComponent from "../components/Home/StatePreparedComponent";
 
 const HomeView = ({ statePending, statePrepared }) => {
+    const [tasks, setTask] = useState([]);
+
+    useEffect(() => {
+        getTask();
+    }, []);
+
+    const getTask = async () => {
+        const response = await axios.get('http://localhost:8000/task/api/list');
+        setTask(response.data);
+    };
+
+    const handleCheckboxChange = (id) => {
+        setTask(prevTasks => 
+            prevTasks.map(task => 
+                task.id === id ? { ...task, completed: !task.completed } : task
+            )
+        );
+    };
 
     return (
         <div className='container mx-auto mt-10'>
@@ -15,6 +36,33 @@ const HomeView = ({ statePending, statePrepared }) => {
                 </div>
                 <div className='md:col-span-2 lg:col-span-3 lg:ml-24'>
                     <h2 className="text-center text-xl font-bold underline mb-4">Lista de tareas</h2>
+                    <div className='overflow-x-auto'>
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Tarea</th>
+                                    <th>Estado</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {tasks.map((task, index) => (
+                                <tr key={task.id} className="hover">
+                                    <td>{index + 1}</td>
+                                    <td>{task.task}</td>
+                                    <td>
+                                        <input
+                                            type="checkbox"
+                                            checked={task.completed}
+                                            onChange={() => handleCheckboxChange(task.id)}
+                                            className="checkbox"
+                                        />
+                                    </td>
+                                </tr>
+                            ))}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
