45c290d56b543aceff2069dd2b434fbaeefc7c5e
LauTortosa <lau.tortosa@gmail.com>
2024-07-28 17:05:25 +0200

    feat: add validation to create planning in 'PlanningFormComponent'

--- frontend/src/components/Planning/PlanningFormComponent.jsx
+++ frontend/src/components/Planning/PlanningFormComponent.jsx
@@ -1,9 +1,12 @@
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import axios from "axios";
+
 import SelectTypeProductComponent from "../Form/SelectTypeProductComponent";
 
 const PlanningFormComponent = () => {
     const { register, handleSubmit, setValue, reset } = useForm();
+    const [error, setError] = useState();
 
 const handleTypeSelect = (typeId, typeLabel) => {
       setValue("type_value", typeLabel);
@@ -15,17 +18,27 @@ const handleProductSelect = (productLabel) => {
 };
 
 const handleSubmitForm = async (data) => {
-    const dataToSend = {
-      date: new Date(data.date).toISOString().split('T')[0],
-      load: parseInt(data.load), 
-      tracebility: parseInt(data.tracebility),
-      type: data.type_value, 
-      product: data.product_value 
-    };
-  
-    const response = await axios.post('http://localhost:8000/planning/api/', dataToSend)
-    console.log('Planificación añadida:', response.data);
-    reset();
+    try {
+        const date = new Date(data.date);
+        if (isNaN(date.getTime())) {
+            throw new Error("Fecha no valida");
+        }
+
+        const dataToSend = {
+            date: date.toISOString().split('T')[0],
+            load: parseInt(data.load), 
+            tracebility: parseInt(data.tracebility),
+            type: data.type_value, 
+            product: data.product_value 
+        };
+
+        await axios.post('http://localhost:8000/planning/api/', dataToSend)
+        reset();
+        setError("");
+    } catch (error) {
+        const errorMessage = error.response?.data?.detail?.[0]?.msg || error.response?.data?.msg || error.message || "Error al añadir la planificación.";
+        setError("Error al añadir la planificación: " + errorMessage);
+    }
 };
 
 return (
@@ -47,7 +60,23 @@ return (
         onProductSelect={handleProductSelect}
       />
       <button type="submit" className="btn">Añadir Planificación</button>
+      {error && <div role="alert" className="alert alert-warning mt-4 mb-4">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 shrink-0 stroke-current"
+                fill="none"
+                viewBox="0 0 24 24">
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+            </svg>
+            <span>{error}</span>
+        </div>}
+
     </form>
+
   );
 };
 
