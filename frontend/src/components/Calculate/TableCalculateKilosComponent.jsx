7ee194669bfb276c604bfb0721a87d61ac06d31b
LauTortosa <lau.tortosa@gmail.com>
2024-08-01 16:36:33 +0200

    feat: integrate sortData function for sorting total kilos in 'TableCaculateKilosComponent'

--- frontend/src/components/Calculate/TableCalculateKilosComponent.jsx
+++ frontend/src/components/Calculate/TableCalculateKilosComponent.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+import { sortData, handleSort } from "../../utils";
+
 const TableCalculateKilosComponent = ({ calculatedKilosTotal }) => {
+    const [sortConfig, setSortConfig] = useState({key: null, direction: 'asc'});
+
+    const sortedTotalKilos = sortData(Object.entries(calculatedKilosTotal).map(([key, value]) => ({ componentName: key, totalKilos: value })), sortConfig);
     return (
         <table className="table">
             <thead>
                 <tr>
                     <th>#</th>
                     <th>Componente</th>
-                    <th>Total kilos</th>
+                    <th className="cursor-pointer" onClick={() => handleSort(sortConfig, setSortConfig, 'totalKilos')}>Total kilos</th>
                 </tr>
             </thead>
             <tbody>
-                {Object.entries(calculatedKilosTotal).map(([componentName, totalKilos], index) => (
+                {sortedTotalKilos.map(({ componentName, totalKilos }, index) => (
                 <tr key={componentName} className="mb-4">
                     <td>{index + 1}</td>
                     <td>{componentName}</td>
