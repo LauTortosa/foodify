fa6cfde939715377eba516ce91ade89fba250c67
LauTortosa <lau.tortosa@gmail.com>
2024-08-03 08:41:54 +0200

    feat: add toggleAllChecks function to check all <th> load

--- frontend/src/components/PlanningDetails/CheckboxLoads.jsx
+++ frontend/src/components/PlanningDetails/CheckboxLoads.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 const CheckboxLoads = ({ components, load, planningId }) => {
     const [checkedLoads, setCheckedLoads] = useState({});
+    const [allChecked, setAllChecked] = useState({});
 
     useEffect(() => {
         const savedData = JSON.parse(localStorage.getItem(`checkedData_${planningId}`));
@@ -11,10 +12,13 @@ const CheckboxLoads = ({ components, load, planningId }) => {
     }, [planningId]);
 
     const saveToLocalStorage = (data) => {
-        const key = `checkedData_${planningId}`;
-        localStorage.setItem(key, JSON.stringify(data));
+        localStorage.setItem(`checkedData_${planningId}`, JSON.stringify(data));
     };
-    
+
+    const isChecked = (componentIndex, loadIndex) => {
+      return checkedLoads[`${componentIndex}_${loadIndex}`] || false;
+    };
+
     const toggleCheck = (componentIndex, loadIndex) => {
         const key = `${componentIndex}_${loadIndex}`;
         setCheckedLoads((prevState) => {
@@ -23,48 +27,56 @@ const CheckboxLoads = ({ components, load, planningId }) => {
             return updatedCheckedLoads;
         });
     };
+
+    const toggleAllChecks = (loadIndex) => {
+      const newCheckedLoads = { ...checkedLoads };
+      const newAllChecked = !allChecked[loadIndex];
+
+      components.forEach((_, componentIndex) => {
+          const key = `${componentIndex}_${loadIndex}`;
+          newCheckedLoads[key] = newAllChecked;
+      });
     
-    const isChecked = (componentIndex, loadIndex) => {
-        const key = `${componentIndex}_${loadIndex}`;
-        return checkedLoads[key] || false;
+      setAllChecked((prevAllChecked) => ({ ...prevAllChecked, [loadIndex]: newAllChecked }));
+      setCheckedLoads(newCheckedLoads);
+      saveToLocalStorage(newCheckedLoads);
     };
 
     return (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Componentes</th>
-              <th>Cantidad</th>
-              {Array.from({ length: load }).map((_, loadIndex) => (
-                <th key={loadIndex}>Carga {loadIndex + 1}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {components.map((component, componentIndex) => {
-              const [ingredient, quantity] = component.split(' - ');
-              return (
-                <tr key={componentIndex}>
-                  <td>{ingredient}</td>
-                  <td>{quantity}</td>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Componentes</th>
+            <th>Cantidad</th>
+            {Array.from({ length: load }).map((_, loadIndex) => (
+            <th key={loadIndex} onClick={() => toggleAllChecks(loadIndex)} className="cursor-pointer">Carga {loadIndex + 1}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {components.map((component, componentIndex) => {
+            const [ingredient, quantity] = component.split(' - ');
+            return (
+              <tr key={componentIndex}>
+                <td>{ingredient}</td>
+                <td>{quantity}</td>
                   {Array.from({ length: load }).map((_, loadIndex) => (
-                    <td key={loadIndex}>
-                      <input
-                        className="ml-4"
-                        type="checkbox"
-                        id={`load_${componentIndex}_${loadIndex}`}
-                        checked={isChecked(componentIndex, loadIndex)}
-                        onChange={() => toggleCheck(componentIndex, loadIndex)}
-                      />
-                    </td>
+                <td key={loadIndex}>
+                  <input
+                    className="ml-4"
+                    type="checkbox"
+                    id={`load_${componentIndex}_${loadIndex}`}
+                    checked={isChecked(componentIndex, loadIndex)}
+                    onChange={() => toggleCheck(componentIndex, loadIndex)}
+                  />
+                </td>
                   ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     );
-    
 }
 
 export default CheckboxLoads; 
