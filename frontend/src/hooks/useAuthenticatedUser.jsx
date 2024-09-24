53ea5343ea8cc5218415ce77a7fa5928837c22f2
Laura <lau.tortosa@gmail.com>
2024-09-24 10:59:34 +0200

    feat: create useAuthenticatedUser hook

---
+++ frontend/src/hooks/useAuthenticatedUser.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+
+const useAuthenticatedUser = () => {
+    const [username, setUsername] = useState('');
+
+    useEffect(()=> {
+        const loadUser = async () => {
+            try {
+                const response = await axios.get('http://localhost:8000/users/api/me', { withCredentials: true });
+                if (response.data.authenticated) {
+                    setUsername(response.data.username);
+                }
+            } catch (error) {
+                console.error('Error checking authentication', error);
+            }
+        };
+
+        loadUser();
+    }, []);
+
+    return username;
+};
+
+export default useAuthenticatedUser;
