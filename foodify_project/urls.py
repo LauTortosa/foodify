09a8234923e32a23a56a81cc7d4e994b7fb76efa
Laura <lau.tortosa@gmail.com>
2024-11-02 14:14:08 +0100

    add url '/'

--- foodify_project/urls.py
+++ foodify_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('product/', include('product.urls')),
