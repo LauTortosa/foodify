be6b71561b2baf476565ee79b7d872a45374abfd
Laura <lau.tortosa@gmail.com>
2024-11-03 13:42:52 +0100

    remove import apiClient

--- frontend/src/hooks/useAuthenticatedUser.jsx
+++ frontend/src/hooks/useAuthenticatedUser.jsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react"
-
-import apiClient from "@/components/apiClient.jsx"
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const useAuthenticatedUser = () => {
     const [username, setUsername] = useState('');
 
     useEffect(()=> {
         const loadUser = async () => {
+            const isDevelopment = import.meta.env.MODE === 'development';
+            const myBaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY;
+
             try {
-                const response = await apiClient.get('/users/api/me', { withCredentials: true });
+                const response = await axios.get(`${myBaseUrl}/users/api/me`, { withCredentials: true });
                 if (response.data.authenticated) {
                     setUsername(response.data.username);
                 }
