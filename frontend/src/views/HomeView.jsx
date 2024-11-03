5c50063e7eb5294813e561c9fb1dea03165f7597
Laura <lau.tortosa@gmail.com>
2024-11-03 09:46:19 +0100

    fix: add alias to @

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 import ListTasksComponent from "@/components/Home/ListTasksComponent";
-import apiClient from "../components/apiClient"
+import apiClient from "@/components/apiClient"
 
 const HomeView = () => {
     const [listTasks, setListTask] = useState([]);
