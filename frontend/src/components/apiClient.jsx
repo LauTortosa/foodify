c6655b3bfc58ebcffc1cf6c08d811e92b5bfdb51
Laura <lau.tortosa@gmail.com>
2024-11-06 16:18:19 +0100

    update config deploy

--- frontend/src/components/apiClient.jsx
+++ frontend/src/components/apiClient.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const isDevelopment = import.meta.env.MODE === 'development';
-const myBaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL : import.meta.env.VITE_API_BASE_URL_DEPLOY;
+
+const myBaseUrl = isDevelopment 
+    ? import.meta.env.VITE_API_BASE_URL_LOCAL 
+    : import.meta.env.VITE_API_BASE_URL_DEPLOY;
 
 const apiClient = axios.create({
     baseURL: myBaseUrl,
