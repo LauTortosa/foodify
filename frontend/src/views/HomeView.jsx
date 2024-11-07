6316ee6b17456d512df4e849ce18d10ad27fe94d
Laura <lau.tortosa@gmail.com>
2024-11-07 16:18:00 +0100

    add log in getTask

--- frontend/src/views/HomeView.jsx
+++ frontend/src/views/HomeView.jsx
@@ -12,6 +12,7 @@ const HomeView = () => {
 
     const getTask = async () => {
         try {
+            console.log("API URL en producción:", import.meta.env.VITE_API_BASE_URL_DEPLOY);
             const response = await apiClient.get('/task/api/list');
             console.log('datos api', response.data);
             setListTask(response.data);
