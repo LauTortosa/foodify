d88e6d80d7236e353619285991613f8f56124c8e
Laura <lau.tortosa@gmail.com>
2024-11-16 16:32:08 +0100

    update settings

--- foodify_project/settings.py
+++ foodify_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'foodify-sesk.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'foodifyplanning.es', 'www.foodifyplanning.es']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
