3cbac9a46873f74a5c0026575a039f5c9c260b6a
Laura <lau.tortosa@gmail.com>
2024-11-18 09:35:39 +0100

    update settings

--- foodify_project/settings.py
+++ foodify_project/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 
 env = environ.Env()
 environ.Env.read_env()
+print("Archivo .env leído correctamente")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
