7199dab00145e3a862f7a1777070592af0a057c0
Laura <lau.tortosa@gmail.com>
2025-05-12 17:40:07 +0200

    style: add 8 cols left col: span-2 with StatePending and PlanningForm right col. span-5 with PlanningList

--- frontend/src/views/PlanningView.jsx
+++ frontend/src/views/PlanningView.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 import PlanningList from '../components/Planning/PlanningList.jsx'
 import PlanningFormComponent from '../components/Planning/PlanningFormComponent.jsx';
 import StatePendingComponent from "../components/Home/StatePendingComponent";
@@ -13,42 +11,44 @@ const PlanningView = ({ statePending, statePrepared }) => {
   const username = useAuthenticatedUser();
 
   return (
-    <div className='container min-h-screen flex flex-col'>
-      <div className='grid grid-cols-1 md:grid-cols-5 gap-8 flex-grow'>
-        <div className='md:col-span-5'>
-          <div className='grid grid-cols-1 md:grid-cols-3 gap-8 mt-8'>
-            <div className='ml-28'>
-              <h2 className='text-md font-bold underline mb-4'>Trabajo pendiente</h2>
-              <StatePendingComponent statePending={statePending} />
-              <StatePreparedComponent statePrepared={statePrepared} />
-            </div>
-            <div>
-              {username === 'responsable' && (
-                <div>
-                  <h2 className='text-lg font-bold underline mb-4'>Acciones</h2>
-                  <button className="btn" onClick={() => document.getElementById('my_modal_3').showModal()}>Añadir planificación</button>
-                  <dialog id="my_modal_3" className="modal">
-                    <div className="modal-box">
-                      <form method="dialog">
-                        <button 
-                          className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-                        >
-                          ✕
-                        </button>
-                      </form>
-                      <h2 className='text-xl text-center font-bold underline mb-4'>Añadir planificación</h2>
-                      <PlanningFormComponent 
-                        refreshPlanningList={listPlanning}
-                      />
-                    </div>
-                  </dialog>
+    <div className="container">
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-8 gap-8">
+        
+        <div className="md:col-span-2 lg:col-span-2 mt-10 ml-8">
+          <h2 className="text-xl font-bold underline mb-4">Trabajo pendiente</h2>
+          <StatePendingComponent statePending={statePending} />
+          <StatePreparedComponent statePrepared={statePrepared} />
+  
+          {username === 'responsable' && (
+            <>
+              <h2 className="text-lg font-bold underline mb-4 mt-6">Acciones</h2>
+              <button className="btn" onClick={() => document.getElementById('my_modal_3').showModal()}>
+                Añadir planificación
+              </button>
+              <dialog id="my_modal_3" className="modal">
+                <div className="modal-box">
+                  <form method="dialog">
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                      ✕
+                    </button>
+                  </form>
+                  <h2 className="text-xl text-center font-bold underline mb-4">Añadir planificación</h2>
+                  <PlanningFormComponent refreshPlanningList={listPlanning} />
                 </div>
-              )}
-            </div>
-          </div>
-          <div className='mt-20 ml-20'>
-            <h2 className="text-center text-xl font-bold underline mb-8">Listado de Planificaciones</h2>
-            <PlanningList plannings={plannings} showLink={true} showState={true} refreshPlanningList={listPlanning} />
+              </dialog>
+            </>
+          )}
+        </div>
+  
+        <div className="md:col-span-1 lg:col-span-6 mt-10 mr-20">
+          <h2 className="text-center text-xl font-bold underline mb-4">Listado de Planificaciones</h2>
+          <div className="overflow-x-auto">
+            <PlanningList
+              plannings={plannings}
+              showLink={true}
+              showState={true}
+              refreshPlanningList={listPlanning}
+            />
           </div>
         </div>
       </div>
