eadfc9e451fcf4cf6d8707182ad3903f0f0f03d5
Laura <lau.tortosa@gmail.com>
2025-05-18 11:09:13 +0200

    feat: add permission for 'responsable' to add and delete events

--- frontend/src/components/Events/EventsComponent.jsx
+++ frontend/src/components/Events/EventsComponent.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+
+import useAuthenticatedUser from "../../hooks/useAuthenticatedUser";
+
 import EventFormComponent from "./EventFormComponent";
 
 const EventsComponent = () => {
     const [events, setEvents] = useState([]);
+    const username = useAuthenticatedUser();
 
     const getEvents = async () => {
         try {
@@ -31,26 +35,29 @@ const EventsComponent = () => {
         <div className="p-4">
             <div className="grid grid-cols-2 items-center mb-4">
                 <h2 className="text-xl font-bold underline text-gray-800">Próximos Eventos</h2>
-                <div>
-                    <button 
-                        onClick={() => document.getElementById('my_modal_3').showModal()} 
-                        className="btn btn-sm btn-ghost text-gray-700 hover:bg-gray-100"
-                    >
-                        ✚ Añadir evento
-                    </button>
-                    
-                    <dialog id="my_modal_3" className="modal">
-                        <div className="modal-box">
-                            <form method="dialog">
-                            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
-                                ✕
-                            </button>
-                            </form>
-                            <h2 className="text-xl text-center font-bold underline mb-4">Añadir evento</h2>
-                            <EventFormComponent refreshEvents={getEvents} />
-                        </div>
-                    </dialog>
-                </div>
+                {username === 'responsable' && (
+                    <div>
+                        <button 
+                            onClick={() => document.getElementById('my_modal_3').showModal()} 
+                            className="btn btn-sm btn-ghost text-gray-700 hover:bg-gray-100"
+                        >
+                            ✚ Añadir evento
+                        </button>
+                        
+                        <dialog id="my_modal_3" className="modal">
+                            <div className="modal-box">
+                                <form method="dialog">
+                                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                                    ✕
+                                </button>
+                                </form>
+                                <h2 className="text-xl text-center font-bold underline mb-4">Añadir evento</h2>
+                                <EventFormComponent refreshEvents={getEvents} />
+                            </div>
+                        </dialog>
+                    </div>
+                )}
+                
             </div>
 
             <ul className="space-y-3">
@@ -61,24 +68,27 @@ const EventsComponent = () => {
                 >
                     <h3 className="text-gray-800 font-medium text-base mb-2 flex justify-between items-center">
                         <span>{event.event}</span>
-                        <button 
-                            onClick={() => deleteEvent(event.id)} 
-                            className="btn btn-square btn-xs">
+                        {username === 'responsable' && ( 
+                            <button 
+                                onClick={() => deleteEvent(event.id)} 
+                                className="btn btn-square btn-xs"
+                            >
                                 <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-4 w-4"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-4 w-4"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
                                 >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M6 18L18 6M6 6l12 12"
-                                />
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M6 18L18 6M6 6l12 12"
+                                    />
                                 </svg>
-                        </button>
+                            </button>
+                        )}
                     </h3>
 
                     <p><span className="font-medium">📅 Día:</span> {event.date_value}</p>
