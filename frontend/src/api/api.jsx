a4c880e0ac780026d0252b9c71eea82bf83ae23d
Laura <lau.tortosa@gmail.com>
2025-06-05 16:56:31 +0200

    fix: update axios

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === 'development'
+    ? import.meta.env.VITE_API_BASE_URL_LOCAL
+    : import.meta.env.VITE_API_BASE_URL_DEPLOY;
+
 const api = axios.create({
-    baseURL: "http://localhost:8000",
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
+  baseURL,
 });
 
 export default api;
